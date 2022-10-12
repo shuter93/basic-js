f42325539d02d8e17811a302abf80db7a676ec2f
shuter93 <shuter.1993@gmail.com>
2022-10-12 16:29:18 +0300

    add 4 functions

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,7 +3,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,9 +14,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let newMatrix = [].concat(...matrix);
+  let count = newMatrix.reduce((res, item) => {
+    if(item == '^^') {
+      res++
+    }
+    return res
+  }, 0)
+  return count;
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -2,18 +2,41 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+
+  if(!date) {
+    return 'Unable to determine the time of year!'
+  }
+
+  try {
+    if (date.getMonth() == 11 || date.getMonth() < 2) {
+      return 'winter'
+    }
+    if (date.getMonth() > 1 & date.getMonth() < 5) {
+      return 'spring'
+    }
+    if (date.getMonth() > 4 & date.getMonth() < 8) {
+      return 'summer'
+    }
+    if (date.getMonth() > 7 & date.getMonth() < 11) {
+      return 'autumn (fall)'
+    }
+  } catch (error) {
+    throw new Error('Invalid date!')
+
+  }
+
+
+
 }
 
 module.exports = {
